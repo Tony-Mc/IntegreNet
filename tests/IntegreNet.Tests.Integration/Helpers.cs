1660594bcbfd4abd0962531ce30022310321e7ec
Tony <Tony-Mc@users.noreply.github.com>
2021-10-13 22:46:40 +0300

    Cleaned up tests

---
+++ tests/IntegreNet.Tests.Integration/Helpers.cs
@@ -0,0 +1,23 @@
+﻿using IntegreNet.Model;
+using Npgsql;
+
+namespace IntegreNet.Tests.Integration
+{
+    internal static class Helpers
+    {
+        public static string CreateConnectionString(Template template)
+        {
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = "localhost",
+                Port = template.Database.Config.Port,
+                Username = template.Database.Config.Username,
+                Password = template.Database.Config.Password,
+                Database = template.Database.Config.Database,
+                Pooling = false // turn pooling off so connections are truly closed, otherwise IntegreSQL will detect open connections and fail to return a test database
+            };
+
+            return builder.ToString();
+        }
+    }
+}
--- tests/IntegreNet.Tests.Integration/Initialization.cs
+++ tests/IntegreNet.Tests.Integration/Initialization.cs
@@ -15,9 +15,9 @@ namespace IntegreNet.Tests.Integration
     public class Initialization
     {
         private const string BaseUrl = "http://localhost:6432/api/";
-
         private IntegreSql _integre;
-        public static string Hash;
+
+        public static string Hash { get; private set; }
 
         [OneTimeSetUp]
         public async Task GlobalSetup()
@@ -32,20 +32,11 @@ namespace IntegreNet.Tests.Integration
             {
                 var template = await _integre.InitializeTemplateAsync(Hash);
 
-                var a = new NpgsqlConnectionStringBuilder
-                {
-                    Host = "localhost",
-                    //Host = template.Database.Config.Host,
-                    Port = template.Database.Config.Port,
-                    Username = template.Database.Config.Username,
-                    Password = template.Database.Config.Password,
-                    Database = template.Database.Config.Database,
-                    Pooling = false // turn pooling off so connections are truly closed, otherwise IntegreSQL will detect open connections and fail to return a test database
-                };
+                var connectionString = Helpers.CreateConnectionString(template);
 
-                await using var conn = new NpgsqlConnection(a.ToString());
+                await using var connection = new NpgsqlConnection(connectionString);
 
-                await conn.ExecuteAsync(schema);
+                await connection.ExecuteAsync(schema);
 
                 await _integre.FinalizeTemplateAsync(Hash);
             }
@@ -68,71 +59,6 @@ namespace IntegreNet.Tests.Integration
             using var hash = MD5.Create();
             return string.Concat(hash.ComputeHash(Encoding.UTF8.GetBytes(text))
                 .Select(x => x.ToString("x2")));
-        }
-    }
-
-    [TestFixture]
-    [Parallelizable(ParallelScope.Children)] // Indicate that tests in this fixture can be run in parallel
-    public class Tests
-    {
-        private const string BaseUrl = "http://localhost:6432/api/";
-
-        private readonly IntegreSql _integre = new(BaseUrl);
-
-        [Test]
-        public async Task Test1()
-        {
-            var database = await _integre.GetTestDatabaseAsync(Initialization.Hash);
-
-            var a = new NpgsqlConnectionStringBuilder
-            {
-                Host = "localhost",
-                Port = database.Database.Config.Port,
-                Username = database.Database.Config.Username,
-                Password = database.Database.Config.Password,
-                Database = database.Database.Config.Database,
-                Pooling = false // turn pooling off so connections are truly closed, otherwise IntegreSQL will detect open connections and fail to return a test database
-            };
-
-            await using var conn = new NpgsqlConnection(a.ToString());
-
-            var countBefore = await conn.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM customers");
-
-            Assert.That(countBefore, Is.EqualTo(3));
-
-            await conn.ExecuteAsync("INSERT INTO customers(name) VALUES ('Joe')");
-
-            var countAfter = await conn.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM customers");
-
-            Assert.That(countAfter, Is.EqualTo(4));
-        }
-
-        [Test]
-        public async Task Test2()
-        {
-            var database = await _integre.GetTestDatabaseAsync(Initialization.Hash);
-
-            var a = new NpgsqlConnectionStringBuilder
-            {
-                Host = "localhost",
-                Port = database.Database.Config.Port,
-                Username = database.Database.Config.Username,
-                Password = database.Database.Config.Password,
-                Database = database.Database.Config.Database,
-                Pooling = false // turn pooling off so connections are truly closed, otherwise IntegreSQL will detect open connections and fail to return a test database
-            };
-
-            await using var conn = new NpgsqlConnection(a.ToString());
-
-            var countBefore = await conn.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM customers");
-
-            Assert.That(countBefore, Is.EqualTo(3));
-
-            await conn.ExecuteAsync("DELETE FROM customers WHERE name LIKE 'S%'");
-
-            var countAfter = await conn.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM customers");
-
-            Assert.That(countAfter, Is.EqualTo(1));
         }
     }
 }
