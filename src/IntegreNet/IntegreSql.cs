8ab136081144f8061d85d0050816224ea7a63586
Tony <Tony-Mc@users.noreply.github.com>
2021-10-30 16:28:17 +0300

    Added docs, sealed some classes

--- src/IntegreNet/IntegreSql.cs
+++ src/IntegreNet/IntegreSql.cs
@@ -9,7 +9,7 @@ using static IntegreNet.Try;
 
 namespace IntegreNet
 {
-    public class IntegreSql
+    public sealed class IntegreSql
     {
         private readonly HttpClient _http;
 
@@ -38,6 +38,18 @@ namespace IntegreNet
             };
         }
 
+        /// <summary>
+        /// Initializes a new template database. This method is intended to be called once per test runner process.
+        /// </summary>
+        /// <remarks>
+        /// After calling <see cref="InitializeTemplateAsync"/> use the returned information to connect to a database, apply schema migrations, seed fixture data and then call <see cref="FinalizeTemplateAsync"/>.
+        /// If you encounter an exception call <see cref="DiscardTemplateAsync"/> to discard a failed template.
+        /// </remarks>
+        /// <param name="hash">The hash of your database migration/fixture files.</param>
+        /// <returns>Initialized database <see cref="Template"/>.</returns>
+        /// <exception cref="TemplateLockedException">Some other process has already recreated a PostgreSQL template database for this hash.</exception>
+        /// <exception cref="ServiceUnavailableException">Service unavailable, there may be connection issues between IntegreSQL and the database.</exception>
+        /// <exception cref="IntegreException">Unexpected status returned.</exception>
         public async Task<Template> InitializeTemplateAsync(string hash)
         {
             return await TryHandle(async () =>
@@ -58,6 +70,14 @@ namespace IntegreNet
             });
         }
 
+        /// <summary>
+        /// Marks database template for provided hash as finalized allowing retrieval of test databases via <see cref="GetTestDatabaseAsync"/>.
+        /// </summary>
+        /// <remarks>
+        /// If you encounter an exception call <see cref="DiscardTemplateAsync"/> to discard a failed template.
+        /// </remarks>
+        /// <param name="hash">The hash of your database migration/fixture files.</param>
+        /// <exception cref="IntegreException">Unexpected status returned.</exception>
         public async Task FinalizeTemplateAsync(string hash)
         {
             await TryHandle(async () =>
@@ -74,6 +94,11 @@ namespace IntegreNet
             });
         }
 
+        /// <summary>
+        /// Discards a template for provided hash.
+        /// </summary>
+        /// <param name="hash">The hash of your database migration/fixture files.</param>
+        /// <exception cref="IntegreException">Unexpected status returned.</exception>
         public async Task DiscardTemplateAsync(string hash)
         {
             await TryHandle(async () =>
@@ -90,6 +115,15 @@ namespace IntegreNet
             });
         }
 
+        /// <summary>
+        /// Retrieves a fully isolated PostgreSQL database from a migrated/seeded template for this hash. Use this database in your test.
+        /// </summary>
+        /// <param name="hash">The hash of your database migration/fixture files.</param>
+        /// <returns>A ready to use database <see cref="Template"/>.</returns>
+        /// <exception cref="TemplateNotFoundException">Template not found. Make sure it is initialized using <see cref="InitializeTemplateAsync"/></exception>
+        /// <exception cref="TemplateDiscardedException">Template was discarded and cannot be used.</exception>
+        /// <exception cref="ServiceUnavailableException">Service unavailable, there may be connection issues between IntegreSQL and the database.</exception>
+        /// <exception cref="IntegreException">Unexpected status returned.</exception>
         public async Task<Template> GetTestDatabaseAsync(string hash)
         {
             return await TryHandle(async () =>
--- src/IntegreNet/Model/Database.cs
+++ src/IntegreNet/Model/Database.cs
@@ -2,11 +2,21 @@
 
 namespace IntegreNet.Model
 {
-    public class Database
+    /// <summary>
+    /// IntegreSQL managed database information.
+    /// </summary>
+    public sealed class Database
     {
+        /// <summary>
+        /// Provided hash for which this database was created.
+        /// </summary>
+        /// <remarks>This allows you to identify the combination of your database migration/fixture files for which this database was created.</remarks>
         [JsonPropertyName("templateHash")]
         public string Hash { get; set; }
 
+        /// <summary>
+        /// Database configuration.
+        /// </summary>
         public DatabaseConfig Config { get; set; }
     }
 }
